ee514f8501fc3b596034e1a4302cb4d1811e6c7a
Eike Spielberg <Eike.Spielberg@uni-due.de>
2025-05-30 08:48:07 +0200

    clean up intendation, retrieve protocol and server name from HTTP Headers

--- setup.php
+++ setup.php
@@ -12,15 +12,15 @@ require_once APPLICATION_ROOT . 'vendor/autoload.php';
 $settings = array();
 $settings['content_settings'] = array();
 $settings['routes'] = array(
-	'admin'          => 'AdminController',
-	'admin/run'      => 'AdminRunController',
-	'admin/survey'   => 'AdminSurveyController',
-	'admin/mail'     => 'AdminMailController',
-	'admin/advanced' => 'AdminAdvancedController',
-    'admin/account'  => 'AdminAccountController',
-    'public'         => 'PublicController',
-	'api'            => 'ApiController',
-	'run'            => 'RunController'
+    'admin' => 'AdminController',
+    'admin/run' => 'AdminRunController',
+    'admin/survey' => 'AdminSurveyController',
+    'admin/mail' => 'AdminMailController',
+    'admin/advanced' => 'AdminAdvancedController',
+    'admin/account' => 'AdminAccountController',
+    'public' => 'PublicController',
+    'api' => 'ApiController',
+    'run' => 'RunController'
 );
 
 // Include helper functions
@@ -33,8 +33,8 @@ require_once APPLICATION_ROOT . 'config/settings.php';
 
 // Define default assets
 if (php_sapi_name() != 'cli') {
-	require_once APPLICATION_ROOT . 'config/assets.php';
-	require_once APPLICATION_ROOT . 'config/css-classes.php';
+    require_once APPLICATION_ROOT . 'config/assets.php';
+    require_once APPLICATION_ROOT . 'config/css-classes.php';
 }
 
 // Set current formr version (bumped on release)
@@ -49,51 +49,68 @@ Config::initialize($settings);
 // Global Setup
 function __formr_setup($settings = array()): void
 {
-	if (defined('WEBROOT')) {
-		return;
-	}
+    if (defined('WEBROOT')) {
+        return;
+    }
 
-	$protocol = (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == '') ? 'http://' : 'https://';
-	$doc_root = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] . '/' : '/';
-	$online = true;
+    if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+        $protocol = $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://';
+    } elseif (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === '' || $_SERVER['HTTPS'] === 'off') {
+        $protocol = 'http://';
+    } else {
+        $protocol = 'https://';
+    }
 
-	if (!empty($settings['protocol'])) {
-		$protocol = $settings['protocol'];
-	}
+    if (!empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+        $server_name = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    } elseif (!empty($_SERVER['SERVER_NAME'])) {
+        $server_name = $_SERVER['SERVER_NAME'];
+    } else {
+        $server_name = '';
+    }
 
-	define('WEBROOT', $protocol . $doc_root);
-	define('SERVER_NAME', isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : '');
-	define('ONLINE', $online);
-	define('SSL', $protocol === "https://");
-	define('RUNROOT', WEBROOT);
-	define('DEBUG', $settings['display_errors']);
-	define('FMRSD_CONTEXT', getenv('FMRSD_CONTEXT'));
+    define('SERVER_NAME', $server_name);
 
-	// General PHP-side configuration
-	error_reporting(-1);
-	if (DEBUG > 0) {
-		ini_set('display_errors', 1);
-	} else {
-		ini_set('display_errors',  0);
- 	}
-	
-	ini_set("log_errors", 1);
-	ini_set("error_log", get_log_file('errors.log'));
-	ini_set('session.gc_maxlifetime', $settings['session_cookie_lifetime']);
-	ini_set('session.cookie_lifetime', $settings['session_cookie_lifetime']);
 
-	// Set cryptography module
-	try {
-		Crypto::setup();
-	} catch (Exception $e) {
-		formr_log_exception($e);
-		formr_error(503, 'Service Unavailable', 'Encryption service unavailable', null, false);
-	}
+    $doc_root = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] . '/' : '/';
+    $online = true;
 
-	// Set default timzone, encoding and shutdown function.
-	date_default_timezone_set($settings['timezone']);
-	mb_internal_encoding('UTF-8');
-	register_shutdown_function('shutdown_formr_org');
+    if (!empty($settings['protocol'])) {
+        $protocol = $settings['protocol'];
+    }
+    define('PROTOCOL', $protocol);
+    define('WEBROOT', $protocol . $doc_root);
+    define('ONLINE', $online);
+    define('SSL', $protocol === "https://");
+    define('RUNROOT', WEBROOT);
+    define('DEBUG', $settings['display_errors']);
+    define('FMRSD_CONTEXT', getenv('FMRSD_CONTEXT'));
+
+    // General PHP-side configuration
+    error_reporting(-1);
+    if (DEBUG > 0) {
+        ini_set('display_errors', 1);
+    } else {
+        ini_set('display_errors', 0);
+    }
+
+    ini_set("log_errors", 1);
+    ini_set("error_log", get_log_file('errors.log'));
+    ini_set('session.gc_maxlifetime', $settings['session_cookie_lifetime']);
+    ini_set('session.cookie_lifetime', $settings['session_cookie_lifetime']);
+
+    // Set cryptography module
+    try {
+        Crypto::setup();
+    } catch (Exception $e) {
+        formr_log_exception($e);
+        formr_error(503, 'Service Unavailable', 'Encryption service unavailable', null, false);
+    }
+
+    // Set default timzone, encoding and shutdown function.
+    date_default_timezone_set($settings['timezone']);
+    mb_internal_encoding('UTF-8');
+    register_shutdown_function('shutdown_formr_org');
 }
 
 // Bootstrap setup
