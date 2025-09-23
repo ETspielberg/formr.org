288f5f69e8f53e09e5c50b079d72e4dd98582fa3
hy1214 <eike.spielberg@uni-due.de>
2025-09-23 07:12:11 +0200

    refactor location of images

--- templates/public/footer.php
+++ templates/public/footer.php
@@ -3,8 +3,8 @@
 <footer>
     <div class="container">
         <p class="float-end"><a href="#" style="color:white">Back to top</a></p>
-        <a href="https://www.uni-due.de/de/index.php" target="_blank"><img class="d-inline-block align-center" height="50" alt="UB-Logo" src="img/logo_ude_weiss.png" style="padding-right:30px;padding-left:10px" height="50"></a>
-        <a href="https://www.uni-due.de/ub/index.php" target="_blank"><img class="d-inline-block align-center" height="50" alt="UB-Logo" src="img/UB_Logo_weiss.png" style="padding-right:30px;padding-left:10px" height="50"></a>
+        <a href="https://www.uni-due.de/de/index.php" target="_blank"><img class="d-inline-block align-center" height="50" alt="UB-Logo" src="assets/common/img/logo_ude_weiss.png" style="padding-right:30px;padding-left:10px" height="50"></a>
+        <a href="https://www.uni-due.de/ub/index.php" target="_blank"><img class="d-inline-block align-center" height="50" alt="UB-Logo" src="assets/common/img/UB_Logo_weiss.png" style="padding-right:30px;padding-left:10px" height="50"></a>
         <a href="/data_protection.php" target="_blank"><img class="d-inline-block align-center" height="50" alt="UB-Logo" src="img/UB_Logo_weiss.png" style="padding-right:30px;padding-left:10px" height="50"></a>
         <span>&copy; UDE &middot; Letzte Änderung: 21.01.2025</span>
     </div>
