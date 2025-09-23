48cc03f525741ff9780341ec8ac5daff5038fe2a
hy1214 <eike.spielberg@uni-due.de>
2025-09-23 14:35:30 +0200

    do not show PHP errors

--- config/settings.php
+++ config/settings.php
@@ -83,7 +83,7 @@ $settings['default_admin_email'] = array(
 
 // should PHP and MySQL errors be displayed to the users when formr is not running locally? If 0, they are only logged
 $settings['display_errors_when_live'] = 0;
-$settings['display_errors'] = 1;
+$settings['display_errors'] = 0;
 $settings['error_to_stderr'] = 1;
 
 // how many emails can be maximally sent to one email address during certain time periods
